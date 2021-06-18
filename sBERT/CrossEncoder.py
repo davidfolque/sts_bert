2ecddec244185981c2fe239f1148e927715bb77a
David Folqué Garcia <david.folque@gmail.com>
2021-06-18 08:26:48 +0100

    Changes to CrossEncoder to allow pretrained nli and different pooling strategies

--- sBERT/CrossEncoder.py
+++ sBERT/CrossEncoder.py
@@ -1,4 +1,4 @@
-from transformers import AutoModel, AutoTokenizer
+from transformers import BertModel, BertTokenizer
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
@@ -9,28 +9,77 @@ import numpy as np
 from Trainer import Trainer
 
 
+def select_from_state_dict(state_dict, key):
+    return {k.split('.', 1)[1]: v for k, v in dict(state_dict).items() if k.split('.', 1)[0] == key}
+
+
 class CrossEncoder(nn.Module):
 
-    def __init__(self, hidden_layer_size=200, sigmoid_temperature=0.1,
-                 model_name='bert-base-uncased',
-                 device='cuda'):
+    # Modes: cls-pooling-hl, mean-pooling-hl, linear-pooling, pretrained-nli
+    def __init__(self, hidden_layer_size=200, sigmoid_temperature=10, mode='cls',
+                 pretrained_nli_label_num=3, device='cuda'):
         super(CrossEncoder, self).__init__()
 
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.bert = AutoModel.from_pretrained(model_name, add_pooling_layer=False)
-        self.hidden_layer = nn.Linear(self.bert.config.hidden_size, hidden_layer_size)
-        self.gelu = nn.GELU()
-        self.output_layer = nn.Linear(hidden_layer_size, 1)
+        assert(mode in ['cls-pooling', 'mean-pooling', 'linear-pooling', 'pretrained_nli'])
+        self.mode = mode
+
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+        if self.mode in ['cls-pooling', 'mean-pooling']:
+            self.hidden_layer = nn.Linear(self.bert.config.hidden_size, hidden_layer_size)
+            self.gelu = nn.GELU()
+            output_layer_in_size = hidden_layer_size
+        elif self.mode == 'linear-pooling':
+            output_layer_in_size = self.bert.config.hidden_size
+        else:
+            assert(self.mode == 'pretrained_nli')
+
+            # Load pretrained model state_dict
+            path = '/home/cs-folq1/rds/rds-t2-cspp025-5bF3aEHVmLU/cs-folq1/pretrained_models/' \
+                   'bert-nli/bert-base.state_dict'
+            print('Loading state dict from ' + path + '.')
+            state_dict = torch.load(path, map_location=device)
+
+            # Load pretrained bert model. Setting strict=False as we don't have position_ids. But
+            # they are not needed here, we can use the default ones.
+            load_result = self.bert.load_state_dict(select_from_state_dict(state_dict, 'bert'),
+                                                    strict=False)
+            # Assert that the only keys missing are the position ids.
+            assert(load_result.missing_keys == ['embeddings.position_ids'])
+            assert(load_result.unexpected_keys == [])
+
+            # Create and load the pretrained nli head.
+            self.nli_head = nn.Linear(self.bert.config.hidden_size, pretrained_nli_label_num)
+            self.nli_head.load_state_dict(select_from_state_dict(state_dict, 'nli_head'))
+            print('Pretrained NLI model successfully loaded.')
+
+            self.softmax = nn.Softmax(dim=1)
+            output_layer_in_size = pretrained_nli_label_num
+
+        self.output_layer = nn.Linear(output_layer_in_size, 1)
         self.sigmoid = nn.Sigmoid()
         self.sigmoid_temperature = sigmoid_temperature
         self.device = device
         self.to(device)
 
     def forward(self, **x):
-        x = self.bert(**x).last_hidden_state  # size = batch x longest_length x emb_size
-        x = x[:, 0, :]  # take the output of [CLS]
-        x = self.gelu(self.hidden_layer(x))
-        x = self.sigmoid(self.sigmoid_temperature * self.output_layer(x))
+        x = self.bert(**x)
+
+        if self.mode == 'cls-pooling':
+            x = x.last_hidden_state[:, 0, :] # Take the output of [CLS].
+            x = self.gelu(self.hidden_layer(x))
+        elif self.mode == 'mean-pooling':
+            x = torch.mean(x.last_hidden_state, dim=1) # Take the mean of all tokens' embeddings.
+            x = self.gelu(self.hidden_layer(x))
+        elif self.mode == 'linear-pooling':
+            x = x.pooler_output
+        else:
+            assert(self.mode == 'pretrained_nli')
+            x = self.softmax(self.nli_head(x.pooler_output))
+
+        x = self.output_layer(x)
+        x = self.sigmoid(x / self.sigmoid_temperature)
         return x
 
 
