04c0e0828d0df5ea0db1661dcbdaa3ae0d7d3a70
David Folqué Garcia <david.folque@gmail.com>
2021-06-25 08:12:54 +0200

    Added script few_shot_paws

---
+++ sBERT/few_shot_paws.py
@@ -0,0 +1,55 @@
+import torch
+
+from Datasets import load_sts, load_paws_wiki
+from CrossEncoder import CrossEncoder, CrossEncoderPretrained
+from STSTrainer import STSTrainer
+from PawsTrainer import PawsTrainer
+from GridRun import grid_run
+
+# Load datasets.
+
+datasets_path = '/home/cs-folq1/rds/rds-t2-cspp025-5bF3aEHVmLU/cs-folq1/datasets/'
+# datasets_path = '/home/david/Documents/PhD/rds/datasets/'
+sts_dataset = load_sts()
+paws_dataset = load_paws_wiki(datasets_path=datasets_path)
+
+
+# Train original cross encoder.
+
+sts_model = CrossEncoder(mode='cls-pooling-hidden')
+sts_trainer = STSTrainer(model=sts_model, train_dataset=sts_dataset['train'],
+                         dataset=sts_dataset, num_epochs=10, lr_scheduler='linear',
+                         warmup_percent=0.2)
+sts_trainer.train(disable_progress_bar=True)
+original_sts_model_path = './saved_models/few_shot_paws/original_cross_encoder.bin'
+sts_model.save_state_dict(original_sts_model_path)
+
+
+# Train on PAWS.
+
+def run_experiment(config):
+    # subset_indices = random_sample(n=len(paws_dataset['train']), k=config['train_size'],
+    #                                seed=config['train_subset_seed'])
+    # train_dataset_subset = torch.utils.data.Subset(paws_dataset['train'], subset_indices)
+
+    sts_model.load_state_dict(torch.load(original_sts_model_path))
+    paws_model = CrossEncoderPretrained(sts_model, mode=config['mode'])
+
+    trainer = PawsTrainer(model=paws_model, dataset=paws_dataset, num_epochs=config['num_epochs'],
+                          batch_size=config['batch_size'], lr=config['lr'])
+    result = trainer.train(disable_progress_bar=True, eval_zero_shot=True)
+    del paws_model
+    torch.cuda.empty_cache()
+    return result
+
+
+grid = {
+    'num_epochs': 10,
+    'batch_size': 16,
+    'lr': 2e-5,
+    'mode': ['replace-head', 'shift-bias', 'additional-head'],
+    # 'train_size': [500, 1000, 2000, 3000, 4000, len(dataset['train'])],
+    'fake_seed': [1, 2, 3]
+}
+
+df_results = grid_run(grid, run_experiment, load_path=None, save_dir='./comparison_results')
