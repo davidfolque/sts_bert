160d13c6e76ba1f9e9c1c21699988c118bbf05da
David Folque Garcia <david.folque@gmail.com>
2021-07-12 07:22:49 +0100

    Changes in few shot paws for bi encoder

--- sBERT/few_shot_paws.py
+++ sBERT/few_shot_paws.py
@@ -1,5 +1,5 @@
 import torch
-
+import os
 from Datasets import load_sts, load_paws_wiki
 from CrossEncoder import CrossEncoder, CrossEncoderPretrained
 from BiEncoder import BiEncoder
@@ -23,7 +23,7 @@ original_nli_sts_cross_model_path = \
 original_sts_bi_model_path = \
     'results/pretraining_sts/results_210707_152607_bi-encoder_base-cls-pooling_best_model.bin'
 original_nli_sts_bi_model_path = \
-    'results/pretraining_sts/results_210707_152607_bi-encoder_nli-cls-pooling_best_model.bin'
+    'results/pretraining_sts/results_210708_103406_bi-encoder_nli-cls-pooling_best_model.bin'
 
 
 # Train on PAWS.
@@ -64,40 +64,51 @@ def run_experiment(config):
             paws_model = CrossEncoderPretrained(sts_model, mode='as-is')
         else:
             assert (pretrained_model in ['sts', 'nli-sts'])
-            paws_model = CrossEncoderPretrained(sts_model, mode='shift-bias')
+            paws_model = CrossEncoderPretrained(sts_model, mode='replace-head')
     else:
         assert (encoder == 'bi')
         if pretrained_model == 'nli':
             paws_model = BiEncoder(mode='nli-cls-pooling', head='extra-head-sub')
         else:
             paws_model = BiEncoder(mode='base-cls-pooling', head='extra-head-sub')
-            if pretrained_model == 'nli-sts':
-                print('Loading nli and sts pretrained model from ' + original_nli_sts_bi_model_path)
-                paws_model.load_state_dict(torch.load(original_nli_sts_bi_model_path))
-            elif pretrained_model == 'sts':
-                print('Loading sts pretrained model from ' + original_sts_bi_model_path)
-                paws_model.load_state_dict(torch.load(original_sts_bi_model_path))
-            else:
-                assert (pretrained_model == 'bert')
+            if pretrained_model != 'bert':
+                if pretrained_model == 'nli-sts':
+                    model_path = original_nli_sts_bi_model_path
+                else:
+                    assert pretrained_model == 'sts'
+                    model_path = original_sts_bi_model_path
+                print('Loading pretrained model from ' + model_path)
+                load_result = paws_model.load_state_dict(torch.load(model_path), strict=False)
+
+                # Assert that the only keys missing are extra head.
+                assert (load_result.missing_keys == ['extra_head.weight', 'extra_head.bias'])
+                assert (load_result.unexpected_keys == [])
 
     trainer = PawsTrainer(model=paws_model, train_dataset=train_dataset_subset,
                           dataset={'dev': dev_dataset_subset, 'test': paws_dataset['test']},
                           num_epochs=config['num_epochs'], batch_size=config['batch_size'],
                           lr=config['lr'])
-    result = trainer.train(disable_progress_bar=True, eval_zero_shot=False)
+    result = trainer.train(disable_progress_bar=True, eval_zero_shot=False, early_stopping=True)
     save_name = 'pretrained_' + config['pretrained_model']
     return result, paws_model, save_name
 
 
-grid = {'num_epochs': 5,  # Size 5000 => 50s per epoch????
+grid = {'num_epochs': 10,  # Size 5000 => 50s per epoch????
         'batch_size': 16, 'lr': 2e-5,
         # 'pretrained_model': ['cross/bert', 'cross/nli-3rd-component', 'cross/sts', 'cross/nli-sts',
         #                      'bi/bert', 'bi/nli', 'bi/sts', 'bi/nli-sts'],
-        'pretrained_model': ['bi/bert', 'bi/nli'],
+        'pretrained_model': ['cross/bert', 'cross/nli', 'cross/sts', 'cross/nli-sts'],
         # 'mode': ['replace-head', 'shift-bias', 'additional-head'],
         # 'mode': 'shift-bias',
         # 'train_size': [500, 1000, 2000, 5000, 10000],
         'train_size': [10, 50, 100, 500, 1000, 2000, 5000, 10000], 'train_subset_seed': [1, 2, 3]}
 
-grid_run = GridRun(run_experiment, results_dir='results', experiment_name='paws_from_nli_sts')
+n_tasks = int(os.environ['SLURM_ARRAY_TASK_COUNT'])
+if n_tasks > 1:
+    task_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
+    grid['pretrained_model'] = grid['pretrained_model'][task_id]
+
+grid_run = GridRun(run_experiment, results_dir='results',
+    experiment_name='paws_from_nli_sts_cross_bert_no_temperature_replace_head2_array_{}'.format(
+        task_id))
 grid_run.run(grid, save_best=False)
