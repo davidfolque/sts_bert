3bf2551fc244cae7cd8fbb09db3430d07df28a39
David Folqué Garcia <david.folque@gmail.com>
2021-07-12 09:43:32 +0200

    Added evaluation during training

--- sBERT/Trainer.py
+++ sBERT/Trainer.py
@@ -72,20 +72,49 @@ class Trainer:
     def train(self, disable_progress_bar, eval_zero_shot=False, early_stopping=True):
         for epoch in range(-1, self.num_epochs):
 
+            # Train epoch.
             if epoch >= 0:
+                # Model in mode train.
                 self.model.train()
+
+                # Prepare variables for evaluating model during training.
+                train_loss = 0
+                train_pred = []
+                train_gold = []
+
                 for batch in tqdm(self.train_dl, disable=disable_progress_bar):
+                    # Zero gradients.
                     self.optimizer.zero_grad()
+
+                    # Forward pass.
                     batch_pred, batch_gold = self.predict_batch(batch)
+
+                    # Remember predictions for evaluation.
+                    train_pred += tolist(batch_pred)
+                    train_gold += tolist(batch_gold)
+
+                    # Compute the loss. Turn gold values into tensors before.
                     if type(batch_gold) == list:
                         batch_gold = torch.FloatTensor(batch_gold)
                     loss = self.loss_function(batch_pred, batch_gold.to(self.model.device))
+
+                    # Store loss for evaluation. Storing sum instead of mean.
+                    train_loss += loss.item() * batch_gold.shape[0]
+
+                    # Backward pass.
                     loss.backward()
+
+                    # Step optimizer, lr_scheduler and debug_function.
                     self.optimizer.step()
                     if self.lr_scheduler is not None:
                         self.lr_scheduler.step()
                     if self.debug_step_function is not None:
                         self.debug_step_function()
+
+                # Print evaluation.
+                train_performance = self.performance(train_pred, train_gold)
+                print('Train epoch {:<4d}: loss: {:<8.4f}, score: {:<8.4f}'.format(
+                    epoch + 1, train_loss / len(batch_pred), train_performance))
 
             if epoch >= 0 or eval_zero_shot:
                 self.model.eval()
@@ -97,7 +126,7 @@ class Trainer:
                     self.best_dev_performance = dev_performance
                     self.best_model_epoch = epoch
                     ast = '*'
-                print('Epoch {:<4d}: loss: {:<8.4f}, score: {:<8.4f}'.format(
+                print('Dev epoch {:<4d}: loss: {:<8.4f}, score: {:<8.4f}'.format(
                     epoch + 1, dev_loss, dev_performance) + ast)
 
                 if early_stopping and epoch + 1 >= (self.num_epochs + 1) // 2 and \
