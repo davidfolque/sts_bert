8bca8a78c7473f8a16df625ad6424f8433d5801d
David Folqué Garcia <david.folque@gmail.com>
2021-07-28 08:47:49 +0200

    Added WikiQABinaryClassifierForALTrainer

--- sBERT/WikiQA/WikiQABinaryClassifierTrainer.py
+++ sBERT/WikiQA/WikiQABinaryClassifierTrainer.py
@@ -7,48 +7,30 @@ from sklearn.metrics import precision_recall_fscore_support, confusion_matrix
 import pandas as pd
 
 
-class WikiQABinaryClassifierTrainer(Trainer):
-    def __init__(self, model, dataset, mode, trainset_size, trainset_seed, num_epochs,
-                 batch_size=16, lr=2e-5, devset_size=None):
-        assert mode in ['all-unscaled', 'all-scaled', 'downsampling']
-        self.mode = mode
+class WikiQABinaryClassifierTrainerBase(Trainer):
+    def __init__(self, *args, **kwargs):
+        self.sigmoid = torch.nn.Sigmoid()
+        super().__init__(*args, **kwargs)
 
-        if self.mode == 'downsampling':
-            train_dl = WikiQAPairsDataLoader(dataset['train'], batch_size=batch_size,
-                                             size=trainset_size, shuffle=True, seed=trainset_seed)
-        else:
-            assert self.mode in ['all-unscaled', 'all-scaled']
-            train_dl = WikiQAAllDataLoader(dataset['train'], batch_size=batch_size,
-                                           size=trainset_size, shuffle=True, seed=trainset_seed)
-
-        dev_dl = WikiQAAllDataLoader(dataset['validation'], batch_size=batch_size, size=devset_size,
-                                     shuffle=devset_size is not None, seed=trainset_seed)
-        test_dl = WikiQAAllDataLoader(dataset['test'], batch_size=batch_size)
-        optimizer = AdamW(model.parameters(), lr=lr)
-
-        if self.mode == 'all-scaled':
+    def update_scaled_loss_function(self, is_scaled):
+        if is_scaled:
             n_pos = 0
             n_neg = 0
-            for batch in train_dl:
+            for batch in self.train_dl:
                 n_pos_batch = (np.array(batch['label']) > 0.5).sum()
                 n_pos += n_pos_batch
                 n_neg += len(batch['label']) - n_pos_batch
             print('+: {}, -: {}'.format(n_pos, n_neg))
 
             def loss_function(outputs, targets):
-                weights = (targets[:, 0] * n_neg + (1 - targets[:, 0]) * n_pos) / (n_neg + n_pos)
+                weights = (targets[:, 0] * n_neg + (1 - targets[:, 0]) * n_pos) / (
+                            n_neg + n_pos)
                 return torch.nn.BCELoss(weights)(outputs, targets[:, 0])
         else:
-            assert self.mode in ['all-unscaled', 'downsampling']
-
             def loss_function(outputs, targets):
                 return torch.nn.BCELoss()(outputs, targets[:, 0])
 
-        super().__init__(model=model, train_dl=train_dl, dev_dl=dev_dl, test_dl=test_dl,
-                         num_epochs=num_epochs, optimizer=optimizer, lr_scheduler='constant',
-                         warmup_percent=0.0, loss_function=loss_function)
-
-        self.sigmoid = torch.nn.Sigmoid()
+        self.loss_function = loss_function
 
     def predict_batch(self, batch):
         logits = self.model.predict_batch(batch['question'], batch['answer'])
@@ -93,3 +75,44 @@ class WikiQABinaryClassifierTrainer(Trainer):
     #             question_start = i + 1
     #             num_questions += 1
     #     return ap_sum / num_questions
+
+
+class WikiQABinaryClassifierTrainer(WikiQABinaryClassifierTrainerBase):
+    def __init__(self, model, dataset, mode, trainset_size, trainset_seed, num_epochs,
+                 batch_size=16, lr=2e-5, devset_size=None):
+        assert mode in ['all-unscaled', 'all-scaled', 'downsampling']
+        self.mode = mode
+
+        if self.mode == 'downsampling':
+            train_dl = WikiQAPairsDataLoader(dataset['train'], batch_size=batch_size,
+                                             size=trainset_size, shuffle=True, seed=trainset_seed)
+        else:
+            assert self.mode in ['all-unscaled', 'all-scaled']
+            train_dl = WikiQAAllDataLoader(dataset['train'], batch_size=batch_size,
+                                           size=trainset_size, shuffle=True, seed=trainset_seed)
+
+        dev_dl = WikiQAAllDataLoader(dataset['validation'], batch_size=batch_size, size=devset_size,
+                                     shuffle=devset_size is not None, seed=trainset_seed)
+        test_dl = WikiQAAllDataLoader(dataset['test'], batch_size=batch_size)
+        optimizer = AdamW(model.parameters(), lr=lr)
+
+        super().__init__(model=model, train_dl=train_dl, dev_dl=dev_dl, test_dl=test_dl,
+                         num_epochs=num_epochs, optimizer=optimizer, lr_scheduler='constant',
+                         warmup_percent=0.0, loss_function=None)
+
+        self.update_scaled_loss_function(self.mode == 'all-scaled')
+
+
+class WikiQABinaryClassifierForALTrainer(WikiQABinaryClassifierTrainerBase):
+    def __init__(self, model, train_dl, dev_dl, test_dl, mode, num_epochs, lr=2e-5):
+        assert mode in ['scaled', 'unscaled']
+        self.mode = mode
+
+        optimizer = AdamW(model.parameters(), lr=lr)
+
+        super().__init__(model=model, train_dl=train_dl, dev_dl=dev_dl, test_dl=test_dl,
+                         num_epochs=num_epochs, optimizer=optimizer, lr_scheduler='constant',
+                         warmup_percent=0.0, loss_function=None)
+
+        assert train_dl.training
+        self.update_scaled_loss_function(self.mode == 'scaled')
